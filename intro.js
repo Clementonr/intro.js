6dbdfdf29c0483de7ab3fe599ea4a05e6555e577
Clementonr <165352284+Clementonr@users.noreply.github.com>
2026-01-18 17:30:26 +0000

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = 'Clement Buabeng Duodu';
+const age =19;
+const favorite Language = 'JavaScript';
+
+console.log('Name: ${Clement}');
+console.log('Age: $(19)' );
+console.log('Favorite Language: ${JavaScript}') ;
